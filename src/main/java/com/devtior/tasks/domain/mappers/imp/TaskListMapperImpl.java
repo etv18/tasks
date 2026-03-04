83c1edc7c509bdd2ff0667c4733e8afab40f15f3
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-03-03 20:36:02 -0400

    implemented toDto function

--- src/main/java/com/devtior/tasks/domain/mappers/imp/TaskListMapperImpl.java
+++ src/main/java/com/devtior/tasks/domain/mappers/imp/TaskListMapperImpl.java
@@ -1,11 +1,15 @@
 package com.devtior.tasks.domain.mappers.imp;
 
 import com.devtior.tasks.domain.dto.TaskListDto;
+import com.devtior.tasks.domain.entities.Task;
 import com.devtior.tasks.domain.entities.TaskList;
+import com.devtior.tasks.domain.entities.TaskStatus;
 import com.devtior.tasks.domain.mappers.TaskListMapper;
 import com.devtior.tasks.domain.mappers.TaskMapper;
 import org.springframework.stereotype.Component;
 
+import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -17,6 +21,7 @@ public class TaskListMapperImpl implements TaskListMapper {
         this.taskMapper = taskMapper;
     }
 
+    /* Turns an object from an instance TaskListDto class to be an instance of TaskList class  */
     @Override
     public TaskList fromDto(TaskListDto taskListDto) {
         return new TaskList(
@@ -35,6 +40,25 @@ public class TaskListMapperImpl implements TaskListMapper {
 
     @Override
     public TaskListDto toDto(TaskList taskList) {
-        return null;
+        return new TaskListDto(
+                taskList.getId(),
+                taskList.getTitle(),
+                taskList.getDescription(),
+                Optional.ofNullable(taskList.getTasks()).map(List::size).orElse(0),
+                calculateTaskListProgress(taskList.getTasks()),
+                Optional.ofNullable(taskList.getTasks()).map(
+                        tasks -> tasks.stream().map(taskMapper::toDto).toList()
+                ).orElse(null)
+        );
+    }
+
+    private Double calculateTaskListProgress(List<Task> tasks) {
+        if(tasks == null) return null;
+
+        long closedTaskCount = tasks.stream().filter(
+                task -> TaskStatus.CLOSED == task.getStatus()
+        ).count();
+
+        return (double ) closedTaskCount / tasks.size();
     }
 }
