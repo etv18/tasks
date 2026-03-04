5f728280a92dc7232b515d9114124282ef3cea8e
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-03-03 23:47:25 -0400

    created task lisk service

---
+++ src/main/java/com/devtior/tasks/domain/services/TaskListService.java
@@ -0,0 +1,10 @@
+package com.devtior.tasks.domain.services;
+
+import com.devtior.tasks.domain.entities.TaskList;
+
+import java.util.List;
+
+public interface TaskListService {
+    List<TaskList> listTaskLists();
+    TaskList createTaskList(TaskList taskList);
+}
