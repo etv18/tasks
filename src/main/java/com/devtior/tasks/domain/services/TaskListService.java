51378c84ff1158215c4fed6fa3eb9ed14ec54a29
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-03-04 21:00:40 -0400

    added getTaskList function

--- src/main/java/com/devtior/tasks/domain/services/TaskListService.java
+++ src/main/java/com/devtior/tasks/domain/services/TaskListService.java
@@ -3,8 +3,11 @@ package com.devtior.tasks.domain.services;
 import com.devtior.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTasklist(UUID id);
 }
