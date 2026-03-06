f227289381e02101ce8fb5237ae95a348278324b
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-03-05 20:07:08 -0400

    Delete function added to task list service implementation

--- src/main/java/com/devtior/tasks/domain/services/imp/TaskListServiceImp.java
+++ src/main/java/com/devtior/tasks/domain/services/imp/TaskListServiceImp.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -46,5 +47,28 @@ public class TaskListServiceImp implements TaskListService {
     @Override
     public Optional<TaskList> getTasklist(UUID id) {
         return taskListRepository.findById(id);
+    }
+
+    @Override
+    public TaskList updateTaskList(UUID taskListId, TaskList taskList) {
+        if(taskList.getId() == null)
+            throw new IllegalArgumentException("Task list must have an ID");
+
+        if(!Objects.equals(taskList.getId(), taskListId))
+            throw new IllegalArgumentException("Attempting to change task list ID, this is not permitted");
+
+        TaskList existingTaskList = taskListRepository.findById(taskListId)
+                .orElseThrow( () -> new IllegalArgumentException("Task list not found") );
+
+        existingTaskList.setTitle(taskList.getTitle());
+        existingTaskList.setDescription(taskList.getDescription());
+        existingTaskList.setUpdatedAt(LocalDateTime.now());
+
+        return taskListRepository.save(existingTaskList);
+    }
+
+    @Override
+    public void deleteTaskList(UUID taskListId) {
+        taskListRepository.deleteById(taskListId);
     }
 }
