7f30e8c8ce0570340afaf99838f1cc98e5e0610d
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-03-06 00:05:10 -0400

    Set up task controller

---
+++ src/main/java/com/devtior/tasks/domain/controllers/TaskController.java
@@ -0,0 +1,68 @@
+package com.devtior.tasks.domain.controllers;
+
+import com.devtior.tasks.domain.dto.TaskDto;
+import com.devtior.tasks.domain.entities.Task;
+import com.devtior.tasks.domain.mappers.TaskMapper;
+import com.devtior.tasks.domain.services.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/api/task-lists/{task_list_id}/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+    public TaskController(TaskService taskService, TaskMapper taskMapper) {
+        this.taskService = taskService;
+        this.taskMapper = taskMapper;
+    }
+
+    @GetMapping
+    public List<TaskDto> listTasks(@PathVariable("task_list_id") UUID taskListId) {
+        return taskService.listTasks(taskListId)
+                .stream()
+                .map(taskMapper::toDto)
+                .toList();
+    }
+
+    @PostMapping
+    public TaskDto createTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @RequestBody TaskDto taskDto
+    ){
+        Task createdTask = taskService.createTask(
+                taskListId,
+                taskMapper.fromDto(taskDto)
+        );
+
+        return taskMapper.toDto(createdTask);
+    }
+
+    @GetMapping(path = "/{task_id}")
+    public Optional<TaskDto> getTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @PathVariable("task_id") UUID taskId
+    ) {
+        return taskService.getTask(taskListId, taskId).map(taskMapper::toDto);
+    }
+
+    @PutMapping(path = "/{task_id}")
+    public TaskDto updateTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @PathVariable("task_id") UUID taskId,
+            @RequestBody TaskDto taskDto
+    ) {
+        Task updatedTask = taskService.updateTask(
+                taskListId,
+                taskId,
+                taskMapper.fromDto(taskDto)
+        );
+
+        return taskMapper.toDto(updatedTask);
+    }
+}
