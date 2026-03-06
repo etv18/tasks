9576ac28e65916628f1afb725e0cdca3a0256554
Emmanuel Tavarez <tavareze2003@gmail.com>
2026-03-06 00:05:19 -0400

    Set up task service

---
+++ src/main/java/com/devtior/tasks/domain/services/TaskService.java
@@ -0,0 +1,14 @@
+package com.devtior.tasks.domain.services;
+
+import com.devtior.tasks.domain.entities.Task;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TaskService {
+    List<Task> listTasks(UUID taskListId);
+    Task createTask(UUID taskListId, Task task);
+    Optional<Task> getTask(UUID taskListId, UUID taskId);
+    Task updateTask(UUID taskListId, UUID taskId, Task task);
+}
